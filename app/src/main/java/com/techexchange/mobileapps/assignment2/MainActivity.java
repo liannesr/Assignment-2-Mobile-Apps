b1b22e24112a05ad413b7ddce8925c19c9e18331
Lianne Sánchez Rodríguez <lianne.sanchez@upr.edu>
2018-10-01 23:33:51 -0700

    Fixed where the logic of the recycler view was located.

--- app/src/main/java/com/techexchange/mobileapps/assignment2/MainActivity.java
+++ app/src/main/java/com/techexchange/mobileapps/assignment2/MainActivity.java
@@ -1,52 +1,25 @@
 package com.techexchange.mobileapps.assignment2;
 
-import android.content.Intent;
-import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 import com.techexchange.mobileapps.assignment2.QuestionListFragment.OnQuestionClickedListener;
 import android.widget.Toast;
-
-import java.io.InputStream;
-import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements OnQuestionClickedListener {
 
-    public static ArrayList<Question> questionsList;
-
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
-
-   // private QuestionListFragmentAdapter myAdapter;
-   // private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        questionsList = getList();
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_questions);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment frag = fm.findFragmentById(R.id.fragment);
 
-        // use a linear layout manager
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        // specify an adapter (see also next example)
-        mAdapter = new ListAdapter(questionsList);
-        mRecyclerView.setAdapter(mAdapter);
-
-       // FragmentManager fm = getSupportFragmentManager();
+        if(frag==null){
+            frag = new QuestionListFragment();
+            fm.beginTransaction().add(R.id.fragment,frag).commit();
+        }
         //viewPager = findViewById(R.id.question_pager);
         //myAdapter = new QuestionListFragmentAdapter(getSupportFragmentManager());
         //myAdapter = new QuestionListFragmentAdapter(getSupportFragmentManager());
@@ -58,50 +31,5 @@ public class MainActivity extends AppCompatActivity implements OnQuestionClicked
     public void onQuestionClicked() {
         Toast.makeText(MainActivity.this, "Wrong!", Toast.LENGTH_SHORT).show();
     }
-
-    public ArrayList<Question> getList(){
-        InputStream ins = getResources().openRawResource(
-                getResources().getIdentifier("countrylist",
-                        "raw", getPackageName()));
-        InputStream ins2 = getResources().openRawResource(
-                getResources().getIdentifier("countrylist",
-                        "raw", getPackageName()));
-        QuestionListFactory.generateCountriesList(ins);
-        return QuestionListFactory.genratingList(ins2);
-    }
-//
-//    private static final class QuestionListFragmentAdapter
-//            extends FragmentStatePagerAdapter {
-//
-//        public QuestionListFragmentAdapter(FragmentManager fm) {
-//            super(fm);
-//        }
-//
-//        @Override
-//        public Fragment getItem(int position) {
-//            Question listQuestion = MainActivity.questionsList.get(position);
-////            Question question = new Question(listQuestion.getQuestion()
-////                    , listQuestion.getCorrectAnswer(), listQuestion.getWrongAnswer());
-//            return QuestionListFragment.createFragmentWith(listQuestion);
-//        }
-//
-//        @Override
-//        public int getCount() {
-//            return questionsList.size();
-//        }
-//
-//
-//    }
-
-
-
-
-
-
-
-
-
-
-
 }
 
--- app/src/main/java/com/techexchange/mobileapps/assignment2/QuestionListFragment.java
+++ app/src/main/java/com/techexchange/mobileapps/assignment2/QuestionListFragment.java
@@ -2,10 +2,16 @@ package com.techexchange.mobileapps.assignment2;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.util.ArrayList;
 
 
 /**
@@ -16,6 +22,9 @@ public class QuestionListFragment extends Fragment {
     private static final String ARG_QUESTION = "ARG_QUESTION";
     private TextView questionView;
     private OnQuestionClickedListener answerListener;
+    private RecyclerView mRecyclerView;
+    private QuestionAdapter qAdapter;
+    public static ArrayList<Question> questionsList;
 
     public QuestionListFragment() {
         // Required empty public constructor
@@ -26,8 +35,15 @@ public class QuestionListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         View rootView = inflater.inflate(R.layout.fragment_question_list, container, false);
         questionView = rootView.findViewById(R.id.question_name);
+
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_questions);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        updateUI();
+
+
         Bundle args = getArguments();
         if(args != null){
             questionView.setText(args.getString(ARG_QUESTION));
@@ -50,6 +66,16 @@ public class QuestionListFragment extends Fragment {
     }
 
 
+    public ArrayList<Question> getList(){
+        InputStream ins = getResources().openRawResource(
+                getResources().getIdentifier("countrylist",
+                        "raw", getActivity().getPackageName()));
+        InputStream ins2 = getResources().openRawResource(
+                getResources().getIdentifier("countrylist",
+                        "raw", getActivity().getPackageName()));
+        QuestionListFactory.generateCountriesList(ins);
+        return QuestionListFactory.genratingList(ins2);
+    }
 
     @Override
     public void onAttach(Context context) {
@@ -62,4 +88,55 @@ public class QuestionListFragment extends Fragment {
         }
     }
 
+    private void updateUI(){
+        questionsList = getList();
+        qAdapter = new QuestionAdapter(questionsList);
+        mRecyclerView.setAdapter(qAdapter);
+    }
+
+    private class QuestionHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private TextView questionTextView;
+        private Question questionIn;
+
+        public QuestionHolder(LayoutInflater inflater, ViewGroup parent){
+            super(inflater.inflate(R.layout.question_list_item,parent,false));
+            itemView.setOnClickListener(this);
+            questionTextView = (TextView) itemView.findViewById(R.id.question_name);
+        }
+
+        public void bind(Question question, int position){
+            questionIn = question;
+            questionTextView.setText("Question #"+position+ "\n"+question.getQuestion().toString());
+        }
+
+        @Override
+        public void onClick(View view){
+            Toast.makeText(getActivity(),questionIn.getQuestion().toString()+" clicked!",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public class QuestionAdapter extends RecyclerView.Adapter<QuestionHolder>{
+        private ArrayList<Question> mQuestions;
+
+        public QuestionAdapter(ArrayList<Question> questions){
+            mQuestions = questions;
+        }
+
+        @Override
+        public QuestionHolder onCreateViewHolder(ViewGroup parent, int viewType){
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            return new QuestionHolder(layoutInflater,parent);
+        }
+
+        @Override
+        public void onBindViewHolder(QuestionHolder holder, int position){
+            Question question = mQuestions.get(position);
+            holder.bind(question,position);
+        }
+
+        @Override
+        public int getItemCount(){
+            return mQuestions.size();
+        }
+    }
 }
